92d16d437dc7c7d1cf2c7b08de648d1a1b626cce
ps-rotk <ps.rotk@gmail.com>
2020-12-11 16:56:26 +0400

    Добавлено описание Таска

--- src/main/Task.java
+++ src/main/Task.java
@@ -1,5 +1,42 @@
 package main;
 
 public class Task {
-    //TODO: заполнить
+    private Integer id;
+    private String date;
+    private String type;
+    private String text;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+
 }
